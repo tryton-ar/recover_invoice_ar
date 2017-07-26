e3ea1d0f4794e04d27a34edaa901712243b4642e
Adrián Bernardi <adrian.bernardi@gmail.com>
2017-07-26 10:17:31 -0300

    Minor adjustments in copyright text

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
-#This file is part of the bank_ar module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part of the recover_invoice_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
 from trytond.pool import Pool
 from .invoice import *
+
 
 def register():
     Pool.register(
