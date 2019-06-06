d781eb494cc3cc6f76e1c2550109a9ffa8c454cd
Luciano Rossi <lukio@gcoop.coop>
2019-06-05 23:08:26 -0300

    fix #7 agrego tests funcional para recover invoice

--- tests/test_recover_invoice_ar.py
+++ tests/test_recover_invoice_ar.py
@@ -1,12 +1,15 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import unittest
+import doctest
 
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import suite as test_suite
+from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(ModuleTestCase):
+class RecoverInvoiceTestCase(ModuleTestCase):
     'Test module'
     module = 'recover_invoice_ar'
 
@@ -14,5 +17,9 @@ class TestCase(ModuleTestCase):
 def suite():
     suite = test_suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            TestCase))
+            RecoverInvoiceTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_recover_invoice_electronic.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
