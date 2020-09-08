9f0394004059bd8db6cf6fa6405f3a1f4d20e1ef
Adrián Bernardi <adrian.bernardi@gmail.com>
2020-09-08 12:55:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ import re
 from configparser import ConfigParser
 from setuptools import setup
 
+MODULE = 'recover_invoice_ar'
+PREFIX = 'trytonar'
 MODULE2PREFIX = {
     'account_invoice_ar': 'trytonar',
     }
@@ -33,7 +35,7 @@ def get_require_version(name):
 
 
 config = ConfigParser()
-config.read_file(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -42,7 +44,6 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonar_recover_invoice_ar'
 
 download_url = 'https://github.com/tryton-ar/recover_invoice_ar/tree/%s.%s' % (
     major_version, minor_version)
@@ -64,7 +65,7 @@ requires.append(get_require_version('trytond'))
 tests_require = [get_require_version('proteus')]
 dependency_links = list(LINKS.values())
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description=('Tryton module for recover electronic invoice from AFIP '
         '(Argentina)'),
@@ -72,13 +73,19 @@ setup(name=name,
     author='tryton-ar',
     url='https://github.com/tryton-ar/recover_invoice_ar',
     download_url=download_url,
-    package_dir={'trytond.modules.recover_invoice_ar': '.'},
+    project_urls={
+        "Bug Tracker": 'https://bugs.tryton.org/',
+        "Documentation": 'https://docs.tryton.org/',
+        "Forum": 'https://www.tryton.org/forum',
+        "Source Code": 'https://github.com/tryton-ar/recover_invoice_ar',
+        },
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.recover_invoice_ar',
-        'trytond.modules.recover_invoice_ar.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.recover_invoice_ar': (info.get('xml', []) + [
+        'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
             'tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
@@ -88,8 +95,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later'
-        ' (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
@@ -110,8 +117,8 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    recover_invoice_ar = trytond.modules.recover_invoice_ar
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
