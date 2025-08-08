fa71768c94af9a1aa876f89bda2363fd016f9f47
Adrián Bernardi <adrian@silix.com.ar>
2025-08-08 20:37:51 -0300

    Deprecate module

--- __init__.py
+++ __init__.py
@@ -1,18 +1,3 @@
 # This file is part of the recover_invoice_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-
-from . import invoice
-
-__all__ = ['register']
-
-def register():
-    Pool.register(
-        invoice.RecoverInvoiceStart,
-        invoice.RecoverInvoiceFactura,
-        module='recover_invoice_ar', type_='model')
-    Pool.register(
-        invoice.RecoverInvoice,
-        module='recover_invoice_ar', type_='wizard')
